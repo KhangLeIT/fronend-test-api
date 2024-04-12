6e9f11bc5b11dfef3a9739ae684cf22940c69ddf
KhangLeIT <lekhangmobile.tm59@gmail.com>
2024-04-12 18:53:08 +0700

    update todo file

--- src/page/TodoPage/TodoPage.js
+++ src/page/TodoPage/TodoPage.js
@@ -3,9 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Select from 'react-select';
 import * as UserServices from '../../services/UserServices';
 import CardTaskComponent from '../../components/CardTaskComponent/CardTaskComponent';
-import { Container, DivNoData, SelectDiv, StyledDiv, TaskContainer } from './style';
-
-
 
 const TodoPage = () => {
   const [selectedOption, setSelectedOption] = useState(null);
@@ -13,7 +10,7 @@ const TodoPage = () => {
   const [error, setError] = useState(null);
 
   const [userTaskDone, setUserTaskDone] = useState([]);
-  const [userTaskNotDone, setUserTaskNotDone] = useState([]);
+  const [userTaskNotDone, setUserTaskNotDone] = useState([])
   const [options, setOptions] = useState([]);
   const [users, setUsers] = useState([]);
 
@@ -21,6 +18,7 @@ const TodoPage = () => {
     setSelectedOption(selectedOption);
     const userId = selectedOption ? selectedOption.id : null;
     fetchTaskUser(userId);
+
   };
 
   const fetchAllUser = async () => {
@@ -41,76 +39,98 @@ const TodoPage = () => {
       const TaskNotDone = res.filter(task => task.completed === false);
       setUserTaskDone(TaskDone)
       setUserTaskNotDone(TaskNotDone)
+
     } catch (error) {
       setError(error.message);
     }
   };
 
   const fetchDoneTask = async (idTask) => {
+    console.log('arr_old',userTaskNotDone)
+    //lấy vị trí click
     const indexToRemove = userTaskNotDone.findIndex(task => task.id === idTask);
+    //lấy dữ liệu tại vị trí click
     const getTaskData = userTaskNotDone[indexToRemove];
+    //cập nhật lại trạng thái
     getTaskData.completed = true
+  
+    //tạo bản sao của chưa hoàn thành
     const newUserTaskNotDone = [...userTaskNotDone];
+    
+    // bỏ phần tử đã chọn ra khỏi chưa hoàn thành
     newUserTaskNotDone.splice(indexToRemove, 1);
+    
+    //thêm dữ liệu vào hoàn thành
     const newUserTaskDone = [getTaskData, ...userTaskDone];
+
+    //cập nhật lại ds chưa hoàn thành
     setUserTaskNotDone(newUserTaskNotDone);
-    setUserTaskDone(newUserTaskDone);
-  };
+    //cập nhật lại ds hoàn thành
+    setUserTaskDone(newUserTaskDone)
+
+    
+  }
+
 
   useEffect(() => {
     fetchAllUser();
   }, []);
 
+
+
+
   useEffect(() => {
-    
-    setOptions(users.map((user) => ({ label: user.name, id: user.id })));
-    
+    if (!loading && !error) {
+      setOptions(users.map((user) => ({ label: user.name, id: user.id })));
+    }
   }, [users]);
 
   return (
-    <Container>
-      <div>User</div>
-      <StyledDiv>
+    <div className='container mt-5'>
+      <div>User</div> 
+      <div className='p-3 border rounded' >
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>Error: {error}</p>
         ) : (
-          <SelectDiv>
+          <div style={{maxWidth: '500px'}}>
             <Select options={options} value={selectedOption} onChange={handleChange}/>
-          </SelectDiv>
+            </div>
+          
         )}
-      </StyledDiv>
-      <div>Tasks</div>
-      <TaskContainer>
-        {userTaskNotDone.length === 0 && userTaskDone.length === 0 ? (
-          <DivNoData>No data</DivNoData>
-        ) : (
-          <>
+      </div>
+      <h4>Task</h4>
+      <div className='p-3 border rounded' style={{ overflowY: 'auto', minHeight: '400px' }}>
+    {userTaskNotDone.length === 0 && userTaskDone.length === 0 ? (
+        <div style={{textAlign:'center'}}>No data</div>
+    ) : (
+        <>
             {userTaskNotDone.map((Task) => (
-              <CardTaskComponent
-                key={Task.id}
-                name={Task.title}
-                namebtn={Task.id}
-                completed={Task.completed}
-                onClick={() => fetchDoneTask(Task.id)}
-              />
+                <CardTaskComponent
+                    key={Task.id}
+                    name={Task.title}
+                    namebtn={Task.id}
+                    completed={Task.completed}
+                    onClick={() => fetchDoneTask(Task.id)}
+                />
             ))}
             {userTaskDone.map((Task) => (
-              <CardTaskComponent
-                key={Task.id}
-                name={Task.title}
-                namebtn={Task.id}
-                completed={Task.completed}
-              />
+                <CardTaskComponent
+                    key={Task.id}
+                    name={Task.title}
+                    namebtn={Task.id}
+                    completed={Task.completed}
+                />
             ))}
-          </>
-        )}
-      </TaskContainer>
+        </>
+    )}
+</div>
+
       <div className='container mt-5'>
         <h4>Done {userTaskDone.length}/{userTaskDone.length+userTaskNotDone.length} Tasks</h4>
-      </div>
-    </Container>
+        </div>
+    </div>
   );
 };
 
